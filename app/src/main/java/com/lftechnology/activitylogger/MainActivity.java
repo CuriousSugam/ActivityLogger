64e8d01254ca63c5aa478c49d7e937f6f3282466
CuriousSugam <sugam19800@gmail.com>
2016-07-06 16:24:51 +0545

    removed imports that were not needed in the main activity

--- app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
+++ app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
@@ -3,20 +3,16 @@ package com.lftechnology.activitylogger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.lftechnology.activitylogger.Controller.SQLiteAccessLayer;
-import com.lftechnology.activitylogger.model.AppDetails;
-
 
 
 public class MainActivity extends AppCompatActivity {
-
-    SQLiteAccessLayer sqLiteAccessLayer;
-    AppDetails appDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 
