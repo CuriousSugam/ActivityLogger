2a76104b5676468402be1cc545effec51cbf292c
CuriousSugam <sugam19800@gmail.com>
2016-07-06 09:48:53 +0545

    create a database to store the app details

---
+++ app/src/main/java/com/lftechnology/activitylogger/Controller/ActivityLoggerSQLiteAccessLayer.java
@@ -0,0 +1,151 @@
+package com.lftechnology.activitylogger.Controller;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.lftechnology.activitylogger.model.AppDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sugam Shakya
+ * Created by Sugam on 7/5/2016.
+ */
+public class ActivityLoggerSQLiteAccessLayer{
+
+    private static final String DBNAME = "ActivityLoggerDatabase";
+    private static final String TABLE_PACKAGE_INFO = "package_info_table";
+    private static final int DATABASE_VERSION = 3;
+
+    private static final String TABLE_COLUMN_ID = "_id";
+    private static final String TABLE_COLUMN_UID = "uid";
+    private static final String TABLE_COLUMN_PACKAGE_NAME = "package_name";
+    private static final String TABLE_COLUMN_APPLICATION_NAME = "application_name";
+
+
+    private ActivityLoggerSQLiteOpenHelper dbHelper;
+    private SQLiteDatabase db;
+    private AppDetails appDetails;
+
+    /**
+     *
+     * @param context Context of the calling activity
+     * @param appDetails Object of AppDetails which holds some data about an application
+     */
+    public ActivityLoggerSQLiteAccessLayer(Context context, AppDetails appDetails){
+        dbHelper = new ActivityLoggerSQLiteOpenHelper(context);
+        this.db = dbHelper.getWritableDatabase();
+        this.appDetails = appDetails;
+    }
+
+    /**
+     *
+     * @param context Context of the calling activity
+     */
+    public ActivityLoggerSQLiteAccessLayer(Context context){
+        dbHelper = new ActivityLoggerSQLiteOpenHelper(context);
+        this.db = dbHelper.getWritableDatabase();
+    }
+
+    /**
+     * This method will insert the data hold by the AppDetails object passed through the constructor
+     * into the database
+     *
+     * @return the row ID of the newly inserted row, or -1 if an error occurred
+     */
+    public long insertIntoAppDetails(){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TABLE_COLUMN_UID, this.appDetails.getUid());
+        contentValues.put(TABLE_COLUMN_PACKAGE_NAME, this.appDetails.getPackageName() );
+        contentValues.put(TABLE_COLUMN_APPLICATION_NAME, this.appDetails.getApplicationName());
+
+        // insert the new row, returning the primary key of the row inserted
+        long newInsertedRowId;
+        newInsertedRowId = db.insert(TABLE_PACKAGE_INFO,null, contentValues);
+
+        return newInsertedRowId;
+    }
+
+    /**
+     *  This method extracts the application information from the database
+     * @return a List<AppDetails>
+     */
+    public List<AppDetails> queryAppDetails(){
+        List<AppDetails> appDetailsList = new ArrayList<>();
+        String[] columns = {TABLE_COLUMN_UID, TABLE_COLUMN_PACKAGE_NAME, TABLE_COLUMN_APPLICATION_NAME};
+        Cursor cursor = db.query(TABLE_PACKAGE_INFO, columns, null, null, null, null, null, null);
+        while(cursor.moveToNext()){
+            AppDetails tempAppDetailsObj = new AppDetails();
+            tempAppDetailsObj.setUid(cursor.getInt(cursor.getColumnIndex(TABLE_COLUMN_UID)));
+            tempAppDetailsObj.setPackageName(cursor.getString(cursor.getColumnIndex(TABLE_COLUMN_PACKAGE_NAME)));
+            tempAppDetailsObj.setApplicationName(cursor.getString(cursor.getColumnIndex(TABLE_COLUMN_APPLICATION_NAME)));
+            appDetailsList.add(tempAppDetailsObj);
+        }
+        cursor.close();
+        return appDetailsList;
+    }
+
+    /**
+     * 
+     *
+     * @param whereClause the WHERE clause to apply when deleting. Passing null will delete all rows.
+     * @param whereArgs  '?'s included in the where clause will be replaced by the values from whereArgs.
+     *                   
+     * deleteAnAppDetail("id=? and name=?",new String[]{"1","jack"});
+     *
+     * @return the number of rows affected if a whereClause is passed in, 0 otherwise.
+     * To remove all rows and get a count pass "1" as the whereClause.
+     */
+    public int deleteAnAppDetail(String whereClause, String[] whereArgs ){
+         return db.delete(TABLE_PACKAGE_INFO, whereClause, whereArgs);
+    }
+
+
+
+
+    class ActivityLoggerSQLiteOpenHelper extends SQLiteOpenHelper {
+
+        public ActivityLoggerSQLiteOpenHelper(Context context) {
+            super(context, DBNAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            // CREATE PACKAGE_INFO_TABLE
+            String query = "Create table "+TABLE_PACKAGE_INFO+"("
+                    +TABLE_COLUMN_ID+" integer primary key autoincrement, "
+                    +TABLE_COLUMN_UID+" integer, "
+                    +TABLE_COLUMN_PACKAGE_NAME+" varchar(255), "
+                    +TABLE_COLUMN_APPLICATION_NAME+" varchar(30));";
+            try{
+                sqLiteDatabase.execSQL(query);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+            //TODO: remove it later
+            Log.e("Database", "Database onCreate method executed");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+            sqLiteDatabase.execSQL("Drop table if exists "+TABLE_PACKAGE_INFO);
+            try{
+                onCreate(sqLiteDatabase);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+            //TODO: remove it later
+            Log.e("Database", "Database onUpgrade method executed");
+        }
+
+    }
+
+}
+
+
--- app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
+++ app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
@@ -1,13 +1,25 @@
 package com.lftechnology.activitylogger;
 
+import android.app.usage.UsageStats;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.lftechnology.activitylogger.Controller.ActivityLoggerSQLiteAccessLayer;
+import com.lftechnology.activitylogger.model.AppDetails;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityLoggerSQLiteAccessLayer sqLiteAccessLayer;
+    AppDetails appDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
