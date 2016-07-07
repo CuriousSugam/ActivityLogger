ae10984772a1ae44b8b51d37c9b6d0ca2b19cce2
darmagedon <sparshadotel@gmail.com>
2016-07-07 13:31:32 +0545

    Shows the list of top 5 used apps

--- app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
+++ app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
@@ -5,13 +5,24 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.lftechnology.activitylogger.Controller.SQLiteAccessLayer;
+import com.lftechnology.activitylogger.model.AppDetails;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter<?> arrayAdapter;
+    List<AppDetails> appDetailsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermissionForAccess();
+        setList();
     }
     /**
      * if you havent enabled permission, settings opens for granting permission access with this
@@ -19,5 +30,10 @@ public class MainActivity extends AppCompatActivity {
     private void checkPermissionForAccess(){
         if(RawAppInfo.getUsageStatsAppList(this).isEmpty())
             startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+    }
+    private void setList(){
+        listView = (ListView)findViewById(R.id.appInfoList);
+        //TODO show the list of apps after put and extracted from DB
+
     }
 }
