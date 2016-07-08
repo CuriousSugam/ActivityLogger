87fdace33e913f26dafcc348a657b85c43f65b12
CuriousSugam <sugam19800@gmail.com>
2016-07-08 12:58:53 +0545

    fetch app details from database in main activity

--- app/src/main/java/com/lftechnology/activitylogger/model/AppDetails.java
+++ app/src/main/java/com/lftechnology/activitylogger/model/AppDetails.java
@@ -1,10 +1,15 @@
 package com.lftechnology.activitylogger.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
 /**
  * @author Sugam Shakya
  * Created by Sugam on 7/5/2016.
+ *
  */
-public class AppDetails {
+public class AppDetails implements Parcelable {
     private int uid;
     private String packageName;
     private String applicationName;
@@ -18,9 +23,40 @@ public class AppDetails {
     public AppDetails(int uid, String packageName, String applicationName){
         setUid(uid);
         setPackageName(packageName);
-        setPackageName(applicationName);
+        setApplicationName(applicationName);
     }
     public AppDetails(){}
+
+    protected AppDetails(Parcel in) {
+        setUid(in.readInt());
+        setPackageName(packageName);
+        setApplicationName(applicationName);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(uid);
+        parcel.writeString(packageName);
+        parcel.writeString(applicationName);
+    }
+
+    public static final Creator<AppDetails> CREATOR = new Creator<AppDetails>() {
+        @Override
+        public AppDetails createFromParcel(Parcel in) {
+            Log.e("AppDetails", "Create from parcel: AppDetails");
+            return new AppDetails(in);
+        }
+
+        @Override
+        public AppDetails[] newArray(int size) {
+            return new AppDetails[size];
+        }
+    };
 
     /**
      *
@@ -69,4 +105,6 @@ public class AppDetails {
     public String getApplicationName() {
         return applicationName;
     }
+
+
 }
