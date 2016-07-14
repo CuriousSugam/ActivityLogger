82d4ebab18b4860276d38d9d44f84df4000affec
CuriousSugam <sugam19800@gmail.com>
2016-07-14 18:04:22 +0545

    main activity layout

--- app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
+++ app/src/main/java/com/lftechnology/activitylogger/MainActivity.java
@@ -8,39 +8,41 @@ import android.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 //import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    ViewPager viewPager;
-    TopSlider topSlider;
+//    ViewPager viewPager;
+//    TopSlider topSlider;
    // LinearLayout l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("check", "welcome to main activity");
 
-        FirstMainFragment frag = new FirstMainFragment();
-        SecondMainFragment frag2 = new SecondMainFragment();
-
-
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.add(R.id.topfragment, frag);
-        fragmentTransaction.commit();
-
-        //l = (LinearLayout)findViewById(R.id.bottom_grid);
-
-        FragmentManager fragmentManager1 = getFragmentManager();
-        FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
-
-        fragmentTransaction1.add(R.id.bottomfragment, frag2);
-        fragmentTransaction1.commit();
-
-        viewPager = (ViewPager)findViewById(R.id.slide_pager);
-        topSlider= new TopSlider(this);
-        viewPager.setAdapter(topSlider);
+//        FirstMainFragment frag = new FirstMainFragment();
+//        SecondMainFragment frag2 = new SecondMainFragment();
+//
+//
+//        FragmentManager fragmentManager = getFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        fragmentTransaction.add(R.id.topfragment, frag);
+//        fragmentTransaction.commit();
+//
+//        //l = (LinearLayout)findViewById(R.id.bottom_grid);
+//
+//        FragmentManager fragmentManager1 = getFragmentManager();
+//        FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
+//
+//        fragmentTransaction1.add(R.id.bottomfragment, frag2);
+//        fragmentTransaction1.commit();
+//
+//        viewPager = (ViewPager)findViewById(R.id.slide_pager);
+//        topSlider= new TopSlider(this);
+//        viewPager.setAdapter(topSlider);
 
     }
 
--- app/src/main/java/com/lftechnology/activitylogger/SecondMainFragment.java
+++ app/src/main/java/com/lftechnology/activitylogger/SecondMainFragment.java
@@ -1,19 +1,38 @@
 package com.lftechnology.activitylogger;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by DevilDewzone on 7/12/2016.
  */
 public class SecondMainFragment extends Fragment {
+    private Context context;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.second_main_fragment,container,false);
+        View view = inflater.inflate(R.layout.second_main_fragment,container,false);
+        Button btnWifi = (Button)view.findViewById(R.id.btn_wifi);
+        btnWifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), WifiActivity.class));
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
     }
 }
