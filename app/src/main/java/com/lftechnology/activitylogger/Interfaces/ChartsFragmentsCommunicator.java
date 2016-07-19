cbe14b6da5433fcc42bffd25461decc84ab15f55
darmagedon <sparshadotel@gmail.com>
2016-07-19 09:56:43 +0545

    Added an Interface to handle inter-fragment communication

---
+++ app/src/main/java/com/lftechnology/activitylogger/Interfaces/ChartsFragmentsCommunicator.java
@@ -0,0 +1,10 @@
+package com.lftechnology.activitylogger.Interfaces;
+
+/**
+ * Created by sparsha on 7/18/2016.
+ */
+public interface ChartsFragmentsCommunicator {
+    void setPieChartImage();
+    void setBarChartImage();
+
+}
