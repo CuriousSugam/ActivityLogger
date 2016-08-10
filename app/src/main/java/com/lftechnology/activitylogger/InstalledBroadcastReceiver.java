9afa80725ab09303cf7261fe0d7da935debf4a2f
Dewesh Rauniyar <devil.dewzone@gmail.com>
2016-08-10 14:05:46 +0545

    replaced statements

--- app/src/main/java/com/lftechnology/activitylogger/InstalledBroadcastReceiver.java
+++ app/src/main/java/com/lftechnology/activitylogger/InstalledBroadcastReceiver.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.nfc.Tag;
-import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
 
+
+import com.lftechnology.activitylogger.Controller.SQLiteAccessLayer;
 import com.lftechnology.activitylogger.Services.InstalledMonitoringService;
+import com.lftechnology.activitylogger.model.AppDetails;
+
+import java.util.List;
 
 /**
  * Created by DevilDewzone on 7/29/2016.
@@ -21,62 +23,37 @@ public class InstalledBroadcastReceiver extends BroadcastReceiver {
 
 
     Context context;
+    //AppDetails appDetails;
+    ApplicationInfo applicationInfo;
+
 
     @Override
     public void onReceive(Context context, Intent aboutAppInfo) {
-        //Log.v(TAG, "Installed BroadCast");
 
         //get app status
         this.context = context;
-        ApplicationInfo applicationInfo = null;
 
         // install uninstall display
         if (aboutAppInfo.getAction().equals("android.intent.action.PACKAGE_ADDED")) {
-            // Log.e("BroadcastReceiver", "onReceive called" + "PACKAGE_INSTALLED");
-            //Toast.makeText(context, "onReceive !!! PACKAGE_INSTALLED", Toast.LENGTH_LONG).show();
-            //applicationInfo = context.getApplicationInfo();
-            //applicationInfo = context.getApplicationInfo().;
-             //applicationInfo = context.getPackageManager().getApplicationInfo(Intent.ACTION_PACKAGE_ADDED,0);
 
-
-            try {
-                    applicationInfo = context.getPackageManager().getApplicationInfo(Intent.ACTION_PACKAGE_ADDED,0);
-
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
             Intent addIntent = new Intent(context, InstalledMonitoringService.class);
-            addIntent.putExtra("AddKey", applicationInfo.name);
+            //addIntent.putExtra("AddKey",);
+            addIntent.putExtra("AddAction", aboutAppInfo.getAction());
             context.startService(addIntent);
 
-        } else if (aboutAppInfo.getAction().equals(Intent.ACTION_PACKAGE_REPLACED)) {
-
-            //Log.e("BroadcastReceiver", "onReceive called" + "PACKAGE_ADDED");
-            //Toast.makeText(context, "onReceive!!! PACKAGE_ADDED", Toast.LENGTH_LONG).show();
-            try {
-                applicationInfo = context.getPackageManager().getApplicationInfo(Intent.ACTION_PACKAGE_REPLACED,0);
-
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
+        } else if (aboutAppInfo.getAction().equals("android.intent.action.PACKAGE_REPLACED")) {
 
             Intent rpIntent = new Intent(context, InstalledMonitoringService.class);
-            rpIntent.putExtra("RpKey", applicationInfo.name);
+            rpIntent.putExtra("RpKey", context.getApplicationInfo().loadLabel(context.getPackageManager()));
+            rpIntent.putExtra("RpAction", aboutAppInfo.getAction());
             context.startService(rpIntent);
 
 
-        } else if (aboutAppInfo.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
-            //Log.e("BroadcastReceiver", "onReceive !!! PACKAGE_REMOVED");
-            //Toast.makeText(context, "onReceive  !!! PACKAGE_REMOVED", Toast.LENGTH_LONG).show();
-            try {
-                applicationInfo = context.getPackageManager().getApplicationInfo(Intent.ACTION_PACKAGE_REMOVED,0);
-
-            } catch (PackageManager.NameNotFoundException e) {
-                e.printStackTrace();
-            }
+        } else if (aboutAppInfo.getAction().equals("android.intent.action.PACKAGE.REMOVED")) {
 
             Intent rmIntent = new Intent(context, InstalledMonitoringService.class);
-            rmIntent.putExtra("RmKey", applicationInfo.name);
+            rmIntent.putExtra("RmKey",context.getApplicationInfo().loadLabel(context.getPackageManager()));
+            rmIntent.putExtra("RmAction", aboutAppInfo.getAction());
             context.startService(rmIntent);
 
         } else {
--- app/src/main/java/com/lftechnology/activitylogger/Services/InstalledMonitoringService.java
+++ app/src/main/java/com/lftechnology/activitylogger/Services/InstalledMonitoringService.java
@@ -3,28 +3,34 @@ package com.lftechnology.activitylogger.Services;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.provider.ContactsContract;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.lftechnology.activitylogger.Controller.SQLiteAccessLayer;
 import com.lftechnology.activitylogger.model.AppDetails;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
- * Created by DevilDewzone on 7/29/2016.
+ * This service performs the action of inserting and deleting
  */
 public class InstalledMonitoringService extends IntentService {
 
 
-    int uid;
-    Context context;
-    AppDetails appDetails ;
-
-
-
+    //int uid;
+    //Context context;
+    AppDetails appDetails;
+    ApplicationInfo applicationInfo;
+    PackageManager packageManager;
     SQLiteAccessLayer accessLayer;
+
+    final Intent insIntent = new Intent(Intent.ACTION_PACKAGE_ADDED, null);
+    final Intent rpIntent = new Intent(Intent.ACTION_PACKAGE_REPLACED, null);
+    final Intent rmIntent = new Intent(Intent.ACTION_PACKAGE_REMOVED, null);
 
 
     public InstalledMonitoringService() {
@@ -32,37 +38,40 @@ public class InstalledMonitoringService extends IntentService {
     }
 
 
-
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleIntent(Intent serviceIntent) {
+        Log.i("check", "message");
+        if (serviceIntent.getStringExtra("AddAction").equals(Intent.ACTION_PACKAGE_ADDED)) {
 
+            Log.e("packageInstallCheck", "inside the intent.actionPackageAdded");
+            //insIntent.addCate
+            //String dbAddIntent = serviceIntent.getStringExtra("AddKey");
+            appDetails = new AppDetails(this.appDetails.getUid(), this.getPackageName(), this.getApplicationInfo().name);
+            accessLayer = new SQLiteAccessLayer(this, appDetails);
+            accessLayer.insertIntoAppDetails();
+            List<AppDetails> listDetails = accessLayer.queryAppDetails();
+            for (AppDetails appListDetails : listDetails) {
+                Log.e("packageInstallCheck", appListDetails.getApplicationName() + appListDetails.getUid() + appListDetails.getPackageName());
+            }
+            accessLayer.closeDatabaseConnection();
 
-        if (intent.equals(Intent.ACTION_PACKAGE_ADDED)) {
-            String dbAddIntent=intent.getStringExtra("AddKey");
-            appDetails= new AppDetails(uid,Intent.ACTION_PACKAGE_ADDED,dbAddIntent);
+        } else if (serviceIntent.getStringExtra("RpAction").equals(Intent.ACTION_PACKAGE_REPLACED)) {
+            String dbRpIntent = serviceIntent.getStringExtra("RpKey");
+            appDetails = new AppDetails(appDetails.getUid(), getPackageName(), dbRpIntent);
             accessLayer = new SQLiteAccessLayer(this, appDetails);
             accessLayer.insertIntoAppDetails();
             accessLayer.closeDatabaseConnection();
 
-
-        } else if (intent.equals(Intent.ACTION_PACKAGE_REPLACED)) {
-            String dbRpIntent=intent.getStringExtra("RpKey");
-            appDetails = new AppDetails(uid,Intent.ACTION_PACKAGE_REPLACED,dbRpIntent);
-            accessLayer = new SQLiteAccessLayer(this,appDetails);
-            accessLayer.insertIntoAppDetails();
-            accessLayer.closeDatabaseConnection();
-
-
-        } else if (intent.equals(Intent.ACTION_PACKAGE_REMOVED)) {
-            String dbRmIntent=intent.getStringExtra("RmKey");
-            appDetails = new AppDetails(uid, Intent.ACTION_PACKAGE_REMOVED,dbRmIntent);
-            accessLayer = new SQLiteAccessLayer(this,appDetails);
-            accessLayer.deleteAnAppDetail("id= and name=android.intent.action.PACKAGE_REMOVED", null);
+        } else if (serviceIntent.getStringExtra("RmAction").equals(Intent.ACTION_PACKAGE_REMOVED)) {
+            String dbRmIntent = serviceIntent.getStringExtra("RmKey");
+            appDetails = new AppDetails(appDetails.getUid(), getPackageName(), dbRmIntent);
+            accessLayer = new SQLiteAccessLayer(this, appDetails);
+            accessLayer.deleteAnAppDetail("?" + "android.intent.action.PACKAGE_REMOVED", new String[]{});
             accessLayer.closeDatabaseConnection();
 
         } else {
             //accessLayer.closeDatabaseConnection();
-            Log.i("Error", "Package Error");
+            Log.i("Error", "Service Error");
         }
     }
 }
