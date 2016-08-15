983dbb59a9e858c5647648e6312e8d00c951b039
CuriousSugam <sugam19800@gmail.com>
2016-08-15 10:38:54 +0545

    click listener to main menu updated

--- app/src/main/java/com/lftechnology/activitylogger/Fragments/SecondMainFragment.java
+++ app/src/main/java/com/lftechnology/activitylogger/Fragments/SecondMainFragment.java
@@ -1,7 +1,5 @@
 package com.lftechnology.activitylogger.fragments;
 
-//import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -10,7 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.lftechnology.activitylogger.AllAppsActivity;
 import com.lftechnology.activitylogger.MainActivity;
@@ -23,6 +21,7 @@ import com.lftechnology.activitylogger.model.AppDetails;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -31,10 +30,17 @@ import butterknife.ButterKnife;
  */
 public class SecondMainFragment extends Fragment {
 
-//    @BindView(R.id.grid_view_main_menu)
-//    GridView gridViewMainMenu;
+    @BindView(R.id.ll_menu_wifi)
+    LinearLayout menuWifi;
 
-    private Context context;
+    @BindView(R.id.ll_menu_all_apps)
+    LinearLayout menuAllApps;
+
+    @BindView(R.id.ll_menu_mobile_data)
+    LinearLayout menuMobileData;
+
+    @BindView(R.id.ll_menu_time)
+    LinearLayout menuTime;
 
     @Nullable
     @Override
@@ -43,15 +49,15 @@ public class SecondMainFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         final List<AppDetails> appDetailsList = getArguments().getParcelableArrayList(MainActivity.APP_DETAILS);
-        TextView txtWifi = (TextView) view.findViewById(R.id.tv_menu_wifi);
-        txtWifi.setOnClickListener(new View.OnClickListener() {
+
+        menuWifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), WifiActivity.class));
             }
         });
-        TextView txtAllApps = (TextView) view.findViewById(R.id.tv_menu_all_apps);
-        txtAllApps.setOnClickListener(new View.OnClickListener() {
+
+        menuAllApps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), AllAppsActivity.class);
@@ -59,29 +65,21 @@ public class SecondMainFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        TextView  txtMobileData= (TextView) view.findViewById(R.id.tv_menu_mobile_data);
-        txtMobileData.setOnClickListener(new View.OnClickListener() {
+
+        menuMobileData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), MobileDataActivity.class));
             }
         });
-        TextView txtTime = (TextView) view.findViewById(R.id.tv_menu_time);
-        txtTime.setOnClickListener(new View.OnClickListener() {
+
+        menuTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), TimeActivity.class));
             }
         });
 
-//        gridViewMainMenu.setAdapter(new MainMenuViewPagerAdapter(context, getResources().getStringArray(R.array.main_menu_items)));
-
         return view;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        this.context = context;
     }
 }
